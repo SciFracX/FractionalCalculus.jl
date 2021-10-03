b7ab5af72afea09f4a325cb6c42cb16119d45ff5
ErikQQY <2283984853@qq.com>
2021-10-03 12:58:43 +0800

    Fix CI not working
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,7 @@
 using Documenter, FractionalCalculus
 
-makedocs(
-    sitename = "FractionalCalculus.jl",
-    author = "Qingyu Qu",
-    clean = true,
-    doctest = false,
-    modules = [FractionalCalculus],
+makedocs()
 
-
-    pages=[
-        "Home" => "index.md",
-    ]
-
-
-    deploydocs(
-        repo = "github.com/ErikQQY/FractionalCalculus.jl.git";
-        push_preview = true
-    )
+deploydocs(
+    repo = "github.com/ErikQQY/FractionalCalculus.jl.git";
 )
