676ede4e200a976b424b3ac9a57042ebd80e1e7c
ErikQQY <2283984853@qq.com>
2022-02-23 23:27:09 +0800

    Add more symbolic fractional derivative rules
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/Derivative/SymbolicDiff.jl
+++ src/Derivative/SymbolicDiff.jl
@@ -6,6 +6,7 @@ abstract type AbstractVariable <: Symbolic end
 const SymbolicVariable = Union{Symbol, AbstractVariable}
 =#
 
+incomplete_beta(z, a, b) = beta(a, b)*beta_inc(a, b, z)[1]
 
 SEMIDIFFRULES = [
     # CONSTANTS AND POWERS
@@ -22,11 +23,15 @@ SEMIDIFFRULES = [
     @rule 1/(1-~x) => (sqrt(1-~x)+sqrt(~x)*asinh(sqrt(~x)))/(sqrt(pi*~x)*(1-~x)^(3/2))
     @rule 1/(1+~x) => (1-~x)/(sqrt(pi*~x)*(1+~x)^2)
     @rule 1/(1-~x) => (1+~x)/(sqrt(pi*~x)*(1-~x)^2)
-    @rule 1/(1-~x)^(~p) => -beta_inc(-1/2, ~p+1/2)/(2*sqrt(pi)*(1-~x)^(~p+1/2))#FIXME: The inputs of incomplete beta function
+    @rule 1/(1-~x)^(~p) => -incomplete_beta(~x, -1/2, ~p+1/2)/(2*sqrt(pi)*(1-~x)^(~p+1/2))
     #FIXME: Elliptic integral functions...
 
     # EXPONENTIAL AND RELATED FUNCTIONS
     @rule exp(~x) => 1/sqrt(pi*~x) + exp(~x)*erf(sqrt(~x))
-    @rule exp(-~x) => 1/sqrt(pi*~x) - 2/sqrt(pi)
+    @rule exp(-~x) => 1/sqrt(pi*~x) - 2/sqrt(pi)*dawson(sqrt(-~x))
     @rule exp(~x)*erf(sqrt(~x)) => exp(~x)
+    @rule dawson(sqrt(~x)) => 1/2*sqrt(pi)*exp(-~x)
+    @rule exp(~x)*erf(sqrt(~x)) => 1/sqrt(pi*~x) - exp(~x)*erf(sqrt(~x))
+    @rule exp(~x)*erf(-sqrt(~x)) => 1/sqrt(pi*~x) + exp(~x)*erf(-sqrt(~x))
+    @rule erf(~x) => exp(-1/2*~x)*besseli(0, 1/2*~x)
 ]
