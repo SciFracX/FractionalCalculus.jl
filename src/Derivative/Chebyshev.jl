a2074aafec63c2b3a455025b1cc0be553d25656d
pedromxavier <pedromxavier@poli.ufrj.br>
2022-09-05 01:19:02 -0300

    Fix formulae

--- src/Derivative/Chebyshev.jl
+++ src/Derivative/Chebyshev.jl
@@ -8,71 +8,86 @@ Compute ``\partial^{\alpha} f(s) `` for ``0 < \alpha < 1``.
 """
 struct ChebyshevSeriesUnit <: ChebyshevDiff end
 
-function chebyshev_U(t::K, n::Integer) where K <: Number
-    U::K = if n == 0
-        1
-    elseif n == 1
-        2t
-    else
-        Uᵢ = 1
-        Uⱼ = 2t
+function _chebyshev_U(t::K, n::Integer) where {K<:Number}
+    if n == 1     # k = 0
+        K[1]
+    elseif n == 2 # k = 0, 1
+        K[1, 2t]
+    else          # k = 0, 1, 2 ... n - 1
+        U = Vector{K}(undef, n)
 
-        for _ = 1:n-1
-            Uᵢ, Uⱼ = (Uⱼ, 2t * Uⱼ - Uᵢ)
+        U[1] = 1
+        U[2] = 2t
+
+        for k = 3:n
+            U[k] = 2t * U[k-1] - U[k-2]
         end
 
-        Uⱼ
+        return U
     end
-
-    return U
 end
 
-function fracdiff(f::Union{Function, K}, α::K, s::K, n::Integer, ::ChebyshevSeriesUnit) where K <: Number
+function fracdiff(f::Union{Function,K}, α::K, s::K, n::Integer, ::ChebyshevSeriesUnit) where {K<:Number}
     @assert 0 < α < 1
 
     # ~ Aliasing ~ #
-    U = chebyshev_U
+    U = _chebyshev_U
 
     # ~ Step 1: f(x); given
     F = f isa K ? (::K) -> f : f
 
     # ~ Step 2: Compute coefficients of expansion into the Chebyshev series; {aₖ}
-    a = zeros(n + 1)
+    a = zeros(K, n + 1)
 
     for k = 0:n
-        τₖ::K = if 1 < k < n
-            2k / n
-        else
-            1 / 2n
+        τ = 0 < k < n ? 2k : 1
+        ρ = 0
+
+        for l = 0:n
+            ρ += if l == 0
+                F(1) / 2
+            elseif l == n
+                (F(-3) * (-1)^k) / 2
+            else
+                F(2 * cos(π * l / n) - 1) * cos((π * k * l) / n)
+            end
         end
-        
-        a[k + 1] = τₖ * sum( F(2 * cos(π * l / n) - 1) * cos(π * k * l / n) for l = 0:n )
+
+        a[k+1] = (τ / n) * ρ
     end
 
     # ~ Step 3: Compute coefficients of expansion into the Chebyshev series; {bₖ}
     b = zeros(K, n + 1)
 
-    for k = n-1:1
-        b[k] = ((4s - 2) * b[k + 1] - (1 - (1 - α) / (k + 2)) * b[k + 2] + (8k + 8) * a[k + 2]) / (1 + (1 - α) / k)
+    for k = (n-1):-1:1
+        b[k] = (
+            2(2s - 1) * b[k+1]
+            - (1 - (1 - α) / (k + 2)) * b[k+2]
+            + 8(k + 1) * a[k+2]
+        ) / (1 + (1 - α) / k)
     end
 
+    # ~ Compute Chebyshev Polynomials
+    U_0 = U(   - 1, n) # ~ x = 0; k = 0, 1, ..., n - 1
+    U_s = U(2s - 1, n) # ~ x = s; k = 0, 1, ..., n - 1
+
     # ~ Step 4: Compute the value of fₙ'(s)
-    f_s::K = 2 * sum((k + 1) * a[k + 2] * U(2s - 1, k + 1) for k = 0:n-1)
+    df_s = 2 * sum((k + 1) * a[k+2] * U_s[k+1] for k = 0:n-1)
 
     # ~ Step 5: Compute the value of F(x) at x = 0 and x = s
-    c = K[((b[k] / 4k) - (b[k + 2] / (4k + 2))) for k = 1:n-1]
+    c = [(b[k] / 4k) - (b[k+2] / 4(k + 2)) for k = 1:n-1]
 
-    F_0::K = sum(c[k] * U(-1, k) for k = 1:n-1)
-    F_s::K = sum(c[k] * U(2s - 1, k) for k = 1:n-1)
+    F_0 = sum([c[k] * U_0[k+1] for k = 1:n-1])
+    F_s = sum([c[k] * U_s[k+1] for k = 1:n-1])
 
     # ~ Step 6: Compute the value of ∂ᵅf(s)
-    return F(0) * s ^ (-α) + (f_s / (1 - α) - F_s + F_0) * s ^ (1 - α)
+    return F(0) * s^(-α) + ((df_s / (1 - α)) - F_s + F_0) * s^(1 - α)
 end
 
 
 @doc raw"""
 """
-function chebyshev_trait(α::T) where T <: Number
+function chebyshev_trait(α::T) where {T<:Number}
     if 0 < α < 1
         return ChebyshevSeriesUnit()
     else
