fb17e461935cf2762603a98fd6873f699e61ca96
pedromxavier <pedromxavier@poli.ufrj.br>
2022-09-05 09:17:00 -0300

    Split function by step

--- src/Derivative/Chebyshev.jl
+++ src/Derivative/Chebyshev.jl
@@ -27,74 +27,99 @@ function _chebyshev_U(t::K, n::Integer) where {K<:Number}
     end
 end
 
-function fracdiff(f::Union{Function,K}, α::K, s::K, n::Integer, ::ChebyshevSeriesUnit) where {K<:Number}
-    @assert 0 < α < 1
-
-    # ~ Aliasing ~ #
-    U = _chebyshev_U
-
-    # ~ Step 1: f(x); given
-    F = f isa K ? (::K) -> f : f
-
-    # ~ Step 2: Compute coefficients of expansion into the Chebyshev series; {aₖ}
-    a = zeros(K, n + 1)
+function _chebyshev_a(::Type{K}, f::Function, n::Integer) where {K<:Number}
+    a = Vector{K}(undef, n + 1)
 
     for k = 0:n
-        τ = 0 < k < n ? 2k : 1
-        ρ = 0
+        ρ = zero(K)
+        τ = 1 < k < n ? 2k : 1
 
         for l = 0:n
-            ρ += if l == 0
-                F(1) / 2
-            elseif l == n
-                (F(-3) * (-1)^k) / 2
+            x::K = 2 * cos(π * l / n) - 1
+            y::K = cos((π * k * l) / n)
+
+            ρ += if 1 < l < n
+                f(x) * y
             else
-                F(2 * cos(π * l / n) - 1) * cos((π * k * l) / n)
+                f(x) * y / 2
             end
         end
 
         a[k+1] = (τ / n) * ρ
     end
 
-    # ~ Step 3: Compute coefficients of expansion into the Chebyshev series; {bₖ}
-    b = zeros(K, n + 1)
-
-    for k = (n-1):-1:1
-        b[k] = (
-            2(2s - 1) * b[k+1]
-            - (1 - (1 - α) / (k + 2)) * b[k+2]
-            + 8(k + 1) * a[k+2]
-        ) / (1 + (1 - α) / k)
-    end
-
-    # ~ Compute Chebyshev Polynomials
-    U_0 = U(   - 1, n) # ~ x = 0; k = 0, 1, ..., n - 1
-    U_s = U(2s - 1, n) # ~ x = s; k = 0, 1, ..., n - 1
-
-    # ~ Step 4: Compute the value of fₙ'(s)
-    df_s = 2 * sum((k + 1) * a[k+2] * U_s[k+1] for k = 0:n-1)
-
-    # ~ Step 5: Compute the value of F(x) at x = 0 and x = s
-    c = [(b[k] / 4k) - (b[k+2] / 4(k + 2)) for k = 1:n-1]
-
-    F_0 = sum([c[k] * U_0[k+1] for k = 1:n-1])
-    F_s = sum([c[k] * U_s[k+1] for k = 1:n-1])
-
-    # ~ Step 6: Compute the value of ∂ᵅf(s)
-    return F(0) * s^(-α) + ((df_s / (1 - α)) - F_s + F_0) * s^(1 - α)
+    return a
 end
 
+function _chebyshev_b(α::K, s::K, a::Vector{K}, n::Integer) where {K<:Number}
+    @assert length(a) == n + 1
 
-@doc raw"""
-"""
-function chebyshev_trait(α::T) where {T<:Number}
+    b = Vector{K}(undef, n + 1)
+
+    b[n+1] = b[n] = 0
+
+    for k = (n-1):-1:1
+        x::K = 2(2s - 1) * b[k+1]
+        y::K = (1 - (1 - α) / (k + 2)) * b[k+2]
+        z::K = 8(k + 1) * a[k+2]
+
+        b[k] = (x - y + z) / (1 + (1 - α) / k)
+    end
+
+    return b
+end
+
+function _chebyshev_c(b::Vector{K}, n::Integer) where {K<:Number}
+    return K[(b[k] / 4k) - (b[k+2] / 4(k + 2)) for k = 1:n-1]
+end
+
+function fracdiff(_f::Union{Function,K}, α::K, s::K, n::Integer, ::ChebyshevSeriesUnit) where {K<:Number}
+    @assert 0 < α < 1
+
+    # ~ Aliasing ~ #
+    U = _chebyshev_U
+
+    # ~ Step 1: f(x); given
+    f = _f isa K ? (::K) -> _f : _f
+
+    # ~ Step 2: Compute coefficients of expansion into the Chebyshev series; {aₖ}
+    a = _chebyshev_a(K, f, n)
+
+    # ~ Step 3: Compute coefficients of expansion into the Chebyshev series; {bₖ}
+    b = _chebyshev_b(α, s, a, n)
+
+    # ~ Compute Chebyshev Polynomials
+    U_0 = U(-1, n) # Uₖ(2x - 1) ~ x = 0; k = 0, 1, ..., n - 1
+    U_s = U(2s - 1, n) # Uₖ(2x - 1) ~ x = s; k = 0, 1, ..., n - 1
+
+    # ~ Step 4: Compute the value of fₙ'(s)
+    df_s = 2 * sum(k * a[k+1] * U_s[k] for k = 1:n)
+
+    # ~ Step 5: Compute the value of F(x) at x = 0 and x = s
+    c = _chebyshev_c(b, n)
+
+    F_0 = c'U_0[2:n]
+    F_s = c'U_s[2:n]
+
+    # ~ Step 6: Compute the value of ∂ᵅf(s)
+    return f(zero(K)) * s^(-α) + ((df_s / (1 - α)) - F_s + F_0) * s^(1 - α)
+end
+
+function _chebyshev_method(α::T) where {T<:Number}
     if 0 < α < 1
         return ChebyshevSeriesUnit()
     else
-        error("No algorithms for α = $(α)")
+        error(
+            """
+            No algorithms for α = $(α). Options are:
+            - ChebyshevSeriesUnit: 0 < α < 1
+            """
+        )
     end
 end
 
 struct ChebyshevSeries <: ChebyshevDiff end
 
-fracdiff(f, α, s, n, ::ChebyshevSeries) = fracdiff(f, α, s, n, chebyshev_trait(α))
+function fracdiff(f::Union{Function,K}, α::K, s::K, n::Integer, ::ChebyshevSeries) where {K<:Number}
+    return fracdiff(f, α, s, n, _chebyshev_method(α))
+end
