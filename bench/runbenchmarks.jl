1303920f8824b9f0d240093bc34c55bdd64673d5
ErikQQY <2283984853@qq.com>
2024-04-06 22:53:13 +0800

    Add benchmarks
    Signed-off-by: ErikQQY <2283984853@qq.com>

---
+++ bench/runbenchmarks.jl
@@ -0,0 +1,16 @@
+using BenchmarkTools: BenchmarkTools, BenchmarkGroup, @btime, @benchmarkable
+using FractionalCalculus
+using Statistics: median
+
+const SUITE = BenchmarkGroup()
+
+testf(x) = x^2
+
+SUITE["Caputo"]["CaputoDiethelm"] = @benchmarkable fracdiff(testf, 0.5, 0.01, CaputoDiethelm())
+SUITE["Caputo"]["CaputoTrap"] = @benchmarkable fracdiff(testf, 0.5, 0.01, CaputoTrap())
+SUITE["Caputo"]["CaputoL1"] = @benchmarkable fracdiff(testf, 0.5, 0.01, CaputoL1())
+
+BenchmarkTools.tune!(SUITE)
+results = BenchmarkTools.run(SUITE; verbose=true)
+
+BenchmarkTools.save(joinpath(@__DIR__, "benchmark_results.json"), median(results))
