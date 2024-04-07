0744025ce60f6d0172ac2b5af1b5fc0451dbd72e
ErikQQY <2283984853@qq.com>
2024-04-07 22:19:49 +0800

    More benchmarks
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- bench/derivative.jl
+++ bench/derivative.jl
@@ -1,9 +1,9 @@
 function benchmark_caputo(tag::String, end_tag::String, step_size::Real)
-    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, step_size, CaputoDiethelm())
-    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, step_size, CaputoL1())
+    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, $step_size, CaputoDiethelm())
+    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, $step_size, CaputoL1())
 end
 
 function benchmark_RL(tag::String, end_tag::String, step_size::Real)
-    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, step_size, RLD())
-    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, step_size, RLDiffL1())
+    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, $step_size, RLD())
+    SUITE["fracdiff"]["cpu"][tag][end_tag] = @benchmarkable fracdiff(x->x^2, 0.5, 1, $step_size, RLDiffL1())
 end
